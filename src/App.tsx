cf1c77942f41a57049d20de303eef9d5f1d8dd5d
Roy test <roypolocarrillo@gmail.com>
2024-12-06 09:58:32 -0500

    cambio de respositorio

---
+++ src/App.tsx
@@ -0,0 +1,112 @@
+import "./App.css";
+/* import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper'; */
+import { Box, Button, TextField } from "@mui/material";
+import axios from "axios";
+import { useState } from "react";
+import TableData from "./tableData";
+
+function App() {
+  const [mto, setMto] = useState("");
+  const [vint, setVint] = useState("");
+  const [npla, setNpla] = useState("");
+  const [data, setData] = useState([]);
+
+  const dataAmort = async () => {
+    try {
+      await axios
+        .post("http://localhost:3005/api/quota", {
+          Mto: mto,
+          Vint: vint,
+          Npla: npla,
+        })
+        .then((res) => {
+          console.log(res.data);
+          setData(data);
+          console.log(data, "🚜🚜");
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "180px",
+          flexDirection: "row",
+          width: "780px",
+          borderRadius: "10px",
+          boxShadow: "0px 0px 10px #403f3e",
+        }}
+      >
+        <Box sx={{ padding: "4px" }}>
+          <TextField
+            type="number"
+            label="Monto"
+            value={mto}
+            size="small"
+            onChange={(e) => setMto(e.target.value)}
+          />
+        </Box>
+        <Box sx={{ padding: "4px" }}>
+          <TextField
+            type="number"
+            size="small"
+            label="Interés"
+            value={vint}
+            onChange={(e) => setVint(e.target.value)}
+          />
+        </Box>
+        <Box sx={{ padding: "4px" }}>
+          <TextField
+            type="number"
+            size="small"
+            label="Meses"
+            value={npla}
+            onChange={(e) => setNpla(e.target.value)}
+          />
+        </Box>
+        <Box
+          sx={{
+            marginTop: "0px",
+            padding: "4px",
+          }}
+        >
+          <Button variant="contained" onClick={dataAmort}>
+            Calcular
+          </Button>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "380px",
+          width: "760px",
+          borderRadius: "10px",
+          marginTop: "20px",
+          overflow: "auto",
+          overflowX: "hidden",
+          padding: "10px",
+          boxShadow: "0px 0px 10px #403f3e",
+          marginBottom: "20px",
+        }}
+      >
+        <TableData />
+      </Box>
+    </>
+  );
+}
+
+export default App;
---
+++ src/tableData.tsx
@@ -0,0 +1,53 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+
+export default function TableData() {
+  function createData(name: string, calories: number, fat: number) {
+    return { name, calories, fat };
+  }
+
+  const rows = [
+    createData("200.000", 9, 6),
+    createData("200.000", 2, 9),
+    createData("200.000", 262, 16),
+    createData("4.200.000", 35, 7),
+    createData("1.200.000", 5, 16.0),
+  ];
+
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Periodos</TableCell>
+              <TableCell align="right">Intereres</TableCell>
+              <TableCell align="right">Meses</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
